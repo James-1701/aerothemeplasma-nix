ad07b95e5388aac6c6e3d035384ca0623b3a6e31
Nia <hakase@litdevs.org>
2026-01-26 12:32:30 +0100

    fix authui7

--- pkgs/plasma/authui7.nix
+++ pkgs/plasma/authui7.nix
@@ -12,6 +12,23 @@ stdenvNoCC.mkDerivation {
     runHook preInstall
     mkdir -p $out/share/plasma/look-and-feel
     cp -r $src/plasma/look-and-feel/authui7 $out/share/plasma/look-and-feel
+
+    # authui7 tries to be clever by loading 'bgtexture.jpg' from a
+    # hardcoded path of the installed SDDM theme, thus assuming the
+    # user has the theme in /usr/share/sddm. let's just copy it over
+    chmod +w $out/share/plasma/look-and-feel/authui7/contents/images
+    cp $src/plasma/sddm/sddm-theme-mod/bgtexture.jpg $out/share/plasma/look-and-feel/authui7/contents/images
+
+    substituteInPlace \
+      $out/share/plasma/look-and-feel/authui7/contents/logout/Logout.qml \
+      $out/share/plasma/look-and-feel/authui7/contents/splash/Splash.qml \
+      --replace-fail "/usr/share/sddm/themes/sddm-theme-mod/bgtexture.jpg" "../images/bgtexture.jpg"
+
+    # and it makes no use of QtMultimedia, so let's just remove it
+    substituteInPlace \
+      $out/share/plasma/look-and-feel/authui7/contents/splash/Splash.qml \
+      --replace-fail "import QtMultimedia" ""
+
     runHook postInstall
   '';
 }
