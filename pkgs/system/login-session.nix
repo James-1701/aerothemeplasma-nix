88132631a1de60862349f8b565642894b3956bb5
Nia <hakase@litdevs.org>
2026-02-05 23:15:53 +0100

    add an LD_PRELOAD to the login session
    thanks to meowkatee for tangentially suggesting this on an upstream MR:
    https://gitgud.io/wackyideas/aerothemeplasma/-/merge_requests/11#note_1759476

--- pkgs/system/login-session.nix
+++ pkgs/system/login-session.nix
@@ -3,6 +3,7 @@
   lib,
   aerothemeplasma,
   kdePackages,
+  libplasma,
   cmake
 }:
 stdenv.mkDerivation {
@@ -14,6 +15,8 @@ stdenv.mkDerivation {
   buildInputs = [ kdePackages.extra-cmake-modules ];
   preConfigure = "cd plasma/sddm/login-sessions";
   postFixup = ''
+    sed -i '/export PLASMA_DEFAULT_SHELL/iexport LD_PRELOAD="${libplasma.out}/lib/libATPlasmaQuick.so ${libplasma.out}/lib/qt-6/qml/io/gitgud/wackyideas/plasma/core/libcorebindingsplugin.so''${LD_PRELOAD:+ }''${LD_PRELOAD}"' $out/bin/startatp-wayland
+
     substituteInPlace $out/bin/startatp-wayland \
       --replace-fail "$out/libexec/plasma-dbus-run-session-if-needed" "${kdePackages.plasma-workspace}/libexec/plasma-dbus-run-session-if-needed" \
       --replace-fail "$out/bin/startplasma-wayland" "${kdePackages.plasma-workspace}/bin/startplasma-wayland"
