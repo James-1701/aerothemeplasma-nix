2dd151ef7290bdd32d562ebb79d5e26553aa2bc8
Nia <hakase@litdevs.org>
2026-02-21 04:57:46 +0100

    launchfeedback: 2025-10-22 -> 2026-02-21

--- flake.nix
+++ flake.nix
@@ -50,12 +50,12 @@
             dimscreenaero = self.callPackage ./pkgs/aeroshell/kwin/dimscreenaero.nix {};
             fadingpopupsaero = self.callPackage ./pkgs/aeroshell/kwin/fadingpopupsaero.nix {};
             flip3d = self.callPackage ./pkgs/aeroshell/kwin/flip3d.nix {};
+            launchfeedback = self.callPackage ./pkgs/aeroshell/kwin/launchfeedback.nix {};
 
             cursors = self.callPackage ./pkgs/aerothemeplasma/assets/cursors.nix {};
             icons = self.callPackage ./pkgs/aerothemeplasma/assets/icons.nix {};
             sounds = self.callPackage ./pkgs/aerothemeplasma/assets/sounds.nix {};
 
-            launchfeedback = self.callPackage ./pkgs/aerothemeplasma/kwin/launchfeedback.nix {};
             loginaero = self.callPackage ./pkgs/aerothemeplasma/kwin/loginaero.nix {};
             smod = self.callPackage ./pkgs/aerothemeplasma/kwin/smod.nix {};
             smodsnap = self.callPackage ./pkgs/aerothemeplasma/kwin/smodsnap.nix {};
--- pkgs/aeroshell/kwin/launchfeedback.nix
+++ pkgs/aeroshell/kwin/launchfeedback.nix
@@ -1,17 +1,17 @@
 {
   stdenv,
-  aerothemeplasma,
+  aeroshell-kwin-repo,
   kdePackages,
   pkg-config,
   cmake,
   lib
 }:
 stdenv.mkDerivation {
-  pname = "aerothemeplasma-launchfeedback";
-  version = "2025-10-22";
-  src = aerothemeplasma;
+  pname = "aeroshell-launchfeedback";
+  version = "2026-02-21";
+  src = aeroshell-kwin-repo;
 
-  preConfigure = "cd kwin/effects_cpp/startupfeedback";
+  preConfigure = "cd effects_cpp/wayland/startupfeedback";
   buildInputs = with kdePackages; [ kwin qttools ];
   nativeBuildInputs = [ cmake pkg-config kdePackages.wrapQtAppsHook ];
   cmakeFlags = [ (lib.cmakeBool "KWIN_BUILD_WAYLAND" true) ];
