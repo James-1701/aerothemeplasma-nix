c1e9b8142d436cc26c119729c4bceb6dac253b98
Hakase <hakase@litdevs.org>
2026-02-23 11:08:41 +0100

    kvantum-windows7aero: 2025-12-30 -> 2026-02-21

--- pkgs/aerothemeplasma/plasma/kvantum-windows7aero.nix
+++ pkgs/aerothemeplasma/plasma/kvantum-windows7aero.nix
@@ -1,17 +1,17 @@
 {
   stdenvNoCC,
-  aerothemeplasma
+  aerothemeplasma-repo
 }:
 stdenvNoCC.mkDerivation {
   pname = "aerothemeplasma-kvantum-windows7aero";
-  version = "2025-12-30";
-  src = aerothemeplasma;
+  version = "2026-02-21";
+  src = aerothemeplasma-repo;
 
   dontUnpack = true;
   installPhase = ''
     runHook preInstall
     mkdir -p $out/share/Kvantum
-    cp -r $src/misc/kvantum/Kvantum/Windows7Aero $out/share/Kvantum
+    cp -r $src/misc/kvantum/Windows7Aero $out/share/Kvantum
     runHook postInstall
   '';
 }
