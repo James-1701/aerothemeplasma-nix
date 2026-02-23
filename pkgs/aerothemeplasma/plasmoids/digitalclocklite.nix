5cf6aff7ea3deb7e2595f5cd6663a7bc7b52e1da
Hakase <hakase@litdevs.org>
2026-02-23 11:49:07 +0100

    digitalclocklite: 2026-02-03 -> 2026-02-21

--- pkgs/aerothemeplasma/plasmoids/digitalclocklite.nix
+++ pkgs/aerothemeplasma/plasmoids/digitalclocklite.nix
@@ -1,11 +1,11 @@
 {
   stdenvNoCC,
-  aerothemeplasma
+  aerothemeplasma-repo
 }:
 stdenvNoCC.mkDerivation {
   pname = "aerothemeplasma-digitalclocklite";
-  version = "2026-02-03";
-  src = aerothemeplasma;
+  version = "2026-02-21";
+  src = aerothemeplasma-repo;
 
   dontUnpack = true;
   installPhase = ''
