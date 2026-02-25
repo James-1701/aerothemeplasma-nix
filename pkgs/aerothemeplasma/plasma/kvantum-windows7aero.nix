82b0fb6c171fb80860d61a1fa9dfa9c036b226ec
Nia <hakase@litdevs.org>
2026-02-25 05:51:54 +0100

    kvantum-windows7aero: add stickies to opaques
    Required for Sevulet Stickies to display properly. Fixes #17.

--- pkgs/aerothemeplasma/plasma/kvantum-windows7aero.nix
+++ pkgs/aerothemeplasma/plasma/kvantum-windows7aero.nix
@@ -10,8 +10,15 @@ stdenvNoCC.mkDerivation {
   dontUnpack = true;
   installPhase = ''
     runHook preInstall
-    mkdir -p $out/share/Kvantum
-    cp -r $src/misc/kvantum/Windows7Aero $out/share/Kvantum
+    mkdir -p $out/share/Kvantum/Windows7Aero
+    cp $src/misc/kvantum/Windows7Aero/Windows7Aero.svg $out/share/Kvantum/Windows7Aero
+
+    # for Sevulet Stickies to not render with a transparent background
+    sed \
+      's/^opaque=.*/&,stickies/' \
+      $src/misc/kvantum/Windows7Aero/Windows7Aero.kvconfig \
+      > $out/share/Kvantum/Windows7Aero/Windows7Aero.kvconfig
+      
     runHook postInstall
   '';
 }
