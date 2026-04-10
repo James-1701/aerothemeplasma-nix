df0a731dd6775af55edb0a322ddd8d60fb285c3c
Nia <hakase@litdevs.org>
2026-04-10 14:04:16 +0200

    kvantum-windows7aero: drop sevulet stickies patch
    This patch isn't relevant anymore since Sevulet Stickies was removed.
    I thought I had removed it when I dropped the Sevulet packages, but
    clearly not, lol.

--- pkgs/aerothemeplasma/plasma/kvantum-windows7aero.nix
+++ pkgs/aerothemeplasma/plasma/kvantum-windows7aero.nix
@@ -11,14 +11,8 @@ stdenvNoCC.mkDerivation {
   installPhase = ''
     runHook preInstall
     mkdir -p $out/share/Kvantum/Windows7Aero
-    cp $src/misc/kvantum/Windows7Aero/Windows7Aero.svg $out/share/Kvantum/Windows7Aero
-
-    # for Sevulet Stickies to not render with a transparent background
-    sed \
-      's/^opaque=.*/&,stickies/' \
-      $src/misc/kvantum/Windows7Aero/Windows7Aero.kvconfig \
-      > $out/share/Kvantum/Windows7Aero/Windows7Aero.kvconfig
-      
+    cp -r $src/misc/kvantum/Windows7Aero $out/share/Kvantum
     runHook postInstall
   '';
+  dontFixup = true;
 }
