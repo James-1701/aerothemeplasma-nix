6b435dd1419c491b25fbccc39ace36ce7995e563
Nia <hakase@litdevs.org>
2026-05-11 08:49:11 +0200

    lucida-console: change message

--- pkgs/external/fonts/lucida-console.nix
+++ pkgs/external/fonts/lucida-console.nix
@@ -11,8 +11,9 @@ stdenvNoCC.mkDerivation {
     hash = "sha256-9N9NbRpp0k4I2irS9FIDtMcUfdqlGHzNcwh5A8o20Xc=";
     message = ''
       Please grab C:\Windows\Fonts\${name} from an up-to-date install of Windows 7.
-      You will likely need to grab more. See https://github.com/nyakase/aerothemeplasma-nix.
-      Once you have the file, run "nix store add-file ${name}", then try again.'';
+      Once you have grabbed the file, run "nix store add-file ${name}", then try again.
+      See https://github.com/nyakase/aerothemeplasma-nix#lucida-console-font for more info.
+    '';
   };
 
   dontUnpack = true;
